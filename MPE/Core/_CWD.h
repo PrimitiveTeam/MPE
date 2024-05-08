86e4dfe7697d8f683fc1626a6093bdc825669ee6
Sebastian <Sebastian.Termen@gmail.com>
2024-05-08 19:00:42 +0300

    Windows only solution to CWD.

--- MPE/Core/_CWD.h
+++ MPE/Core/_CWD.h
@@ -2,13 +2,30 @@
 
 #include "MPE/Core/_CORE.h"
 
+/**
+ * @file _CWD.h
+ * @brief Set the current working directory to the executable path.
+ * @details This file is used to set the current working directory to the executable path.
+ * @date 2024-05-05
+ * @author Sebastian Termen
+ */
+
+/**
+ * @def SET_EXECUTABLE_PATH_AS_CWD()
+ * @brief Set the current working directory to the executable path.
+ * @details This macro is used to set the current working directory to the executable path.
+ * @date 2024-05-05
+ * @note This macro is only available on Windows. For now.
+ */
+
+// TODO: Add support for Linux and MacOS
+#ifdef MPE_PLATFORM_WINDOWS
+
 #include <string>
 #include <windows.h>
 #include <Shlwapi.h>
 #pragma comment(lib, "Shlwapi.lib")
 
-// TODO: Add support for Linux and MacOS
-#ifdef MPE_PLATFORM_WINDOWS
 #define SET_EXECUTABLE_PATH_AS_CWD()                              \
     {                                                             \
         char path[MAX_PATH];                                      \
