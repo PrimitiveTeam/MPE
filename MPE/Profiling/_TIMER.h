bf946665fe72a063f2a63353c3892cbda6ec4257
Sebastian <Sebastian.Termen@gmail.com>
2024-05-08 19:12:34 +0300

    Documented _TIMER and _PROFILING.

--- MPE/Profiling/_TIMER.h
+++ MPE/Profiling/_TIMER.h
@@ -5,33 +5,86 @@
 #include <chrono>
 #include <functional>
 
+/**
+ * @file _TIMER.h
+ * @brief Timer class for profiling.
+ * @date 2024-05-05
+ * @author Sebastian Termen
+ */
+
 namespace MPE
 {
+    /**
+     * @brief Timer class for profiling.
+     * @details This class is used to profile code execution time.
+     * @tparam FN The function type to call on stopping the timer.
+     * @date 2024-05-05
+     * @see _PROFILING
+     */
     template <typename FN = std::function<void(const char *, float)>>
     class _TIMER
     {
     private:
+        /**
+         * @brief The name of the timer.
+         * @date 2024-05-05
+         */
         const char *NAME;
+        /**
+         * @brief The function to call on stopping the timer.
+         * @date 2024-05-05
+         */
         FN FUNC;
+        /**
+         * @brief The starting point of the timer.
+         * @date 2024-05-05
+         */
         std::chrono::time_point<std::chrono::high_resolution_clock> START_POINT;
+        /**
+         * @brief The stopping flag of the timer.
+         * @date 2024-05-05
+         */
         bool STOPPED;
+        /**
+         * @brief The logging flag of the timer.
+         * @date 2024-05-05
+         */
         bool LOG;
 
     public:
-        // Constructor with only the name - no action after stopping
+        /**
+         * @brief Construct a new _TIMER object.
+         * @details This constructor is used to create a new _TIMER object with only the name.
+         * @param name The name of the timer.
+         * @param log The logging flag of the timer.
+         * @date 2024-05-05
+         */
         _TIMER(const char *name, bool log = false)
             : NAME(name), FUNC(nullptr), STOPPED(false), LOG(log)
         {
             START_POINT = std::chrono::high_resolution_clock::now();
         }
 
-        // Constructor with a name and a function to call on stopping
+        /**
+         * @brief Construct a new _TIMER object.
+         * @details This constructor is used to create a new _TIMER object with the name and a function to call on stopping.
+         * @param name The name of the timer.
+         * @param func The function to call on stopping the timer.
+         * @param log The logging flag of the timer.
+         * @date 2024-05-05
+         */
         _TIMER(const char *name, FN &&func, bool log = false)
             : NAME(name), FUNC(std::move(func)), STOPPED(false), LOG(log)
         {
             START_POINT = std::chrono::high_resolution_clock::now();
         }
 
+        /**
+         * @brief Destroy the _TIMER object.
+         * @date 2024-05-05
+         * @note If the timer is not stopped, it will stop it.
+         * @see Stop()
+         */
         ~_TIMER()
         {
             if (!STOPPED)
@@ -40,6 +93,13 @@ namespace MPE
             }
         }
 
+        /**
+         * @brief Stop the timer.
+         * @details This function is used to stop the timer and execute the function if provided.
+         * @date 2024-05-05
+         * @note If the timer is already stopped, it will not stop it again.
+         * @see FUNC
+         */
         void Stop()
         {
             if (!STOPPED)
