fbcea6139eca8ded47ba9196b1ec5b5cad93e89c
Sebastian <Sebastian.Termen@gmail.com>
2024-05-09 21:18:01 +0300

    First functional test.

--- Tests/MPE/Core/Logging/LogTest.cpp
+++ Tests/MPE/Core/Logging/LogTest.cpp
@@ -7,51 +7,34 @@ namespace MPE_TEST
     class Logger_Log_Test : public ::testing::Test
     {
     protected:
-        std::stringstream buffer;
-        std::streambuf *oldCoutStreamBuf;
+        MPE::Log *logger;
 
         void SetUp() override
         {
-            MPE::GlobalLog::Init();
-
-            // Redirect cout to buffer
-            // oldCoutStreamBuf = std::cout.rdbuf();
-            // std::cout.rdbuf(buffer.rdbuf());
+            logger = new MPE::Log("TestLogger", MPE::Log::MEMORY);
         }
 
         void TearDown() override
         {
-            // Reset cout to its old self
-            // std::cout.rdbuf(oldCoutStreamBuf);
-        }
-
-        std::string getLoggedOutput()
-        {
-            return buffer.str();
+            delete logger;
         }
     };
 
     // Test to check if the correct log message is printed to console
-    // TEST_F(Logger_Log_Test, CoreInfoLogTest)
-    // {
-    //     MPE_CORE_INFO("This is an information log.");
-    //     std::string loggedOutput = getLoggedOutput();
-    //     EXPECT_TRUE(loggedOutput.find("This is an information log.") != std::string::npos);
-    // }
-
-    // Test to check if the correct log message is printed to console
-    TEST_F(Logger_Log_Test, CoreInfoLogTest2)
+    TEST_F(Logger_Log_Test, CoreInfoLogTest)
     {
-        MPE_CORE_INFO("This is an information log.");
-        std::string loggedOutput = getLoggedOutput();
-        EXPECT_TRUE(true);
+        logger->info("This is an information log.");
+        std::string loggedOutput = logger->getOutput();
+        EXPECT_TRUE(loggedOutput.find("This is an information log.") != std::string::npos);
+        MPE_CORE_INFO("Sink: {0}", loggedOutput);
     }
 
 }
 
 int main(int argc, char **argv)
 {
-    // MPE::GlobalLog::Init();
+    // Init can only be called once per source file
+    MPE::GlobalLog::Init();
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
