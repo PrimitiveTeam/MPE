dbee6b1fc64af2ca56bd667dff5e8f12bfab77cc
Sebastian <Sebastian.Termen@gmail.com>
2024-05-12 13:07:43 +0300

    Cleanup.

--- MPE/Events/EventFunction.h
+++ MPE/Events/EventFunction.h
@@ -11,18 +11,10 @@ namespace MPE
         std::string FunctionName;
 
     public:
-        // FunctionCalledEvent(std::string functionName)
-        //     : FunctionName(functionName) {}
         FunctionCalledEvent(std::string functionName);
 
         inline std::string GetFunctionName() const { return FunctionName; }
 
-        // std::string ToString() const override
-        // {
-        //     std::stringstream ss;
-        //     ss << "FunctionCalledEvent: " << FunctionName;
-        //     return ss.str();
-        // }
         std::string ToString() const override;
 
         EVENT_CLASS_TYPE(FunctionCalled)
