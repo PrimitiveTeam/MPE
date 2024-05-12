29c9aef4fbabb5e41860a35d3620ae268378d171
Sebastian Termen <sebastian.termen@gmail.com>
2024-05-12 19:54:24 +0300

    CWD for Linux.

--- MPE/Core/_CWD.h
+++ MPE/Core/_CWD.h
@@ -15,10 +15,10 @@
  * @brief Set the current working directory to the executable path.
  * @details This macro is used to set the current working directory to the executable path.
  * @date 2024-05-05
- * @note This macro is only available on Windows. For now.
+ * @note This macro is only available on Windows and UNIX systems.
  */
 
-// TODO: Add support for Linux and MacOS
+// TODO: Add support for MacOS
 #ifdef MPE_PLATFORM_WINDOWS
 
 #include <string>
@@ -40,6 +40,37 @@
             MPE_CORE_ERROR("Cannot set the working directory");   \
         }                                                         \
     }
+
+#elif MPE_PLATFORM_LINUX
+
+#include <string>
+#include <unistd.h>
+#include <limits.h>
+#include <libgen.h>
+
+#define SET_EXECUTABLE_PATH_AS_CWD()                                  \
+    {                                                                 \
+        char path[PATH_MAX];                                          \
+        ssize_t count = readlink("/proc/self/exe", path, PATH_MAX);   \
+        if (count != -1)                                              \
+        {                                                             \
+            path[count] = '\0';                                       \
+            dirname(path);                                            \
+            if (chdir(path) == 0)                                     \
+            {                                                         \
+                MPE_CORE_WARN("Working directory set to: {0}", path); \
+            }                                                         \
+            else                                                      \
+            {                                                         \
+                MPE_CORE_ERROR("Cannot set the working directory");   \
+            }                                                         \
+        }                                                             \
+        else                                                          \
+        {                                                             \
+            MPE_CORE_ERROR("Failed to retrieve executable path");     \
+        }                                                             \
+    }
+
 #else
-#error CWD currently supports only Windows.
+#error CWD does not support this system.
 #endif
