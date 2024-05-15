019a1a2a368e23fe8588ca2770259c510bc04a6e
Sebastian Termen <sebastian.termen@unity3d.com>
2024-05-15 20:27:05 +0300

    Removed this from PCH as now we will have platform specific PCH.

--- MPE/MPEPCH.h
+++ MPE/MPEPCH.h
@@ -27,15 +27,3 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-
-#ifdef MPE_PLATFORM_WINDOWS
-#    include <Windows.h>
-#endif
-
-#ifdef MPE_PLATFORM_LINUX or MPE_PLATFORM_OSX
-#    include <unistd.h>
-#endif
-
-#ifdef MPE_PLATFORM_OSX
-#    include <Cocoa/Cocoa.h>
-#endif
