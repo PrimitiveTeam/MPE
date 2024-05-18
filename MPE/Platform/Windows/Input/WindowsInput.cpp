72fdd92e1cd91a3ada67e309f97c5a95db8ab7e1
Sebastian <Sebastian.Termen@gmail.com>
2024-05-18 17:37:44 +0300

    Enabled Input for Windows.

---
+++ MPE/Platform/Windows/Input/WindowsInput.cpp
@@ -0,0 +1,52 @@
+#include "WindowsInput.h"
+
+#include "MPE/MPEPCH.h"
+#include "MPE/App/App.h"
+
+#include <GLFW/glfw3.h>
+
+namespace MPE
+{
+	// bool InitializeInput() {
+    //     REF<Input> Input::SYS_Input_Instance = std::make_shared<WindowsInput>();
+    //     return true;
+    // }
+REF<Input> Input::SYS_Input_Instance = NEWREF<WindowsInput>();
+// REF<Input> Input::SYS_Input_Instance = NEWREF<WindowsInput>();
+// REF<Input> Input::SYS_Input_Instance = std::make_shared<WindowsInput>();
+
+bool WindowsInput::IsKeyPressedImpl(int keycode)
+{
+    auto window = static_cast<GLFWwindow *>(App::GetApp()->GetWindow()->GetNativeWindow());
+    auto state = glfwGetKey(window, keycode);
+    return state == GLFW_PRESS || state == GLFW_REPEAT;
+}
+
+bool WindowsInput::IsMouseButtonPressedImpl(int button)
+{
+    auto window = static_cast<GLFWwindow *>(App::GetApp()->GetWindow()->GetNativeWindow());
+    auto state = glfwGetMouseButton(window, button);
+    return state == GLFW_PRESS;
+}
+
+std::pair<float, float> WindowsInput::GetMousePositionImpl()
+{
+    auto window = static_cast<GLFWwindow *>(App::GetApp()->GetWindow()->GetNativeWindow());
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+
+    return {(float) xpos, (float) ypos};
+}
+
+float WindowsInput::GetMouseXImpl()
+{
+    auto [x, y] = GetMousePositionImpl();
+    return x;
+}
+
+float WindowsInput::GetMouseYImpl()
+{
+    auto [x, y] = GetMousePositionImpl();
+    return y;
+}
+}
