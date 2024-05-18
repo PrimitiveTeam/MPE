ef92365287f715277e59126ea5fffdb55fc63768
Sebastian Termen <sebastian.termen@unity3d.com>
2024-05-19 01:36:35 +0300

    Made sure to check macOS OpenGL version.

--- MPE/Platform/OpenGL/OpenGLContext.cpp
+++ MPE/Platform/OpenGL/OpenGLContext.cpp
@@ -26,9 +26,15 @@ void OpenGLContext::Init()
     glGetIntegerv(GL_MAJOR_VERSION, &OpenGLVersionMajor);
     glGetIntegerv(GL_MINOR_VERSION, &OpenGLVersionMinor);
 
-    MPE_CORE_ASSERT(OpenGLVersionMajor > 4 || (OpenGLVersionMajor == 4 && OpenGLVersionMinor >= 6), "MPE REQUIRES OPENGL VERSION 4.6 OR GREATER.");
-
     MPE_CORE_INFO("OpenGL Version: {0}.{1}", OpenGLVersionMajor, OpenGLVersionMinor);
+
+#ifdef MPE_PLATFORM_WINDOWS
+    MPE_CORE_ASSERT(OpenGLVersionMajor > 4 || (OpenGLVersionMajor == 4 && OpenGLVersionMinor >= 6), "MPE REQUIRES OPENGL VERSION 4.6 OR GREATER.");
+#elif MPE_PLATFORM_OSX
+    MPE_CORE_ASSERT(OpenGLVersionMajor >= 3 || (OpenGLVersionMajor >= 3 && OpenGLVersionMinor >= 2), "MPE REQUIRES OPENGL VERSION 3.2 FOR macOS.");
+#else
+    MPE_CORE_ASSERT(OpenGLVersionMajor > 4 || (OpenGLVersionMajor == 4 && OpenGLVersionMinor >= 6), "MPE REQUIRES OPENGL VERSION 4.6 OR GREATER.");
+#endif
 
 #ifdef MPE_ENABLE_DEBUG_LOG
     MPE_CORE_INFO("Vendor: {0}", reinterpret_cast<const char *>(glGetString(GL_VENDOR)));
