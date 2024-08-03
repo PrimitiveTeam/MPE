5ba1cdaa1b7c10264cf2fab9327c2c9777aa81a9
Sebastian <Sebastian.Termen@gmail.com>
2024-08-03 18:07:00 +0300

    Using .cmake instead of CMakeLists.txt for projects

---
+++ Projects/projects.cmake
@@ -0,0 +1,24 @@
+# DUMMY
+if(MPE_PROJECT_DUMMY)
+    include("${PROJECT_SOURCE_DIR}/Projects/DUMMY/dummy.cmake")
+endif(MPE_PROJECT_DUMMY)
+
+# SANDBOX
+if(MPE_PROJECT_SANDBOX)
+    include("${PROJECT_SOURCE_DIR}/Projects/Sandbox/sandbox.cmake")
+endif(MPE_PROJECT_SANDBOX)
+
+# PROJECT PICKER
+if(MPE_PROJECT_PROJECTPICKER)
+    include("${PROJECT_SOURCE_DIR}/Projects/ProjectPicker/projectpicker.cmake")
+endif(MPE_PROJECT_PROJECTPICKER)
+
+# PONG
+if(MPE_PROJECT_PONG)
+    include("${PROJECT_SOURCE_DIR}/Projects/Pong/pong.cmake")
+endif(MPE_PROJECT_PONG)
+
+# OPENCV TEST
+if(MPE_PROJECT_OPENCVTEST)
+    include("${PROJECT_SOURCE_DIR}/Projects/OpenCVTest/opencvtest.cmake")
+endif(MPE_PROJECT_OPENCVTEST)
