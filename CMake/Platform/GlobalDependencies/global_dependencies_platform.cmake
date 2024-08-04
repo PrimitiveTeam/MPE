7d6a7533c76031dfcdf9c6f492a4d328bd6b42d4
Sebastian Termen <sebastian.termen@gmail.com>
2024-08-04 23:55:14 +0300

    fixed cmake build if checks for windows. made sure to not allow the user to enable X11 and wayland at the same time.

--- CMake/Platform/GlobalDependencies/global_dependencies_platform.cmake
+++ CMake/Platform/GlobalDependencies/global_dependencies_platform.cmake
@@ -1,6 +1,11 @@
-if(WIN32)
+if(COMPILATION_PLATFORM STREQUAL "WIN32")
     include("${PROJECT_SOURCE_DIR}/CMake/Platform/GlobalDependencies/WIN32/global_dependencies_win32.cmake")
 elseif(COMPILATION_PLATFORM STREQUAL "UNIX")
+    # DO AN ADDITIONAL CHECK FOR X11 AND WAYLAND - MOST LIKELY CAN'T BE USED TOGETHER
+    if(MPE_USE_X11 AND MPE_USE_WAYLAND)
+        message(FATAL_ERROR "X11 AND WAYLAND CAN'T BE USED TOGETHER.")
+    endif()
+
     include("${PROJECT_SOURCE_DIR}/CMake/Platform/GlobalDependencies/UNIX/global_dependencies_unix.cmake")
 elseif(COMPILATION_PLATFORM STREQUAL "OSX")
     include("${PROJECT_SOURCE_DIR}/CMake/Platform/GlobalDependencies/OSX/global_dependencies_osx.cmake")
