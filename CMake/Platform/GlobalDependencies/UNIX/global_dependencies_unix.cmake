1b1babd254a11ce21764ce450e45fad6b327256f
Sebastian Termen <sebastian.termen@gmail.com>
2024-08-04 23:56:20 +0300

    added angle support for linux

--- CMake/Platform/GlobalDependencies/UNIX/global_dependencies_unix.cmake
+++ CMake/Platform/GlobalDependencies/UNIX/global_dependencies_unix.cmake
@@ -3,7 +3,38 @@ find_package(spdlog CONFIG REQUIRED)
 find_package(glfw3 CONFIG REQUIRED)
 find_package(glm CONFIG REQUIRED)
 
-if(${VERBOSE})
+if(MPE_USE_X11)
+  find_package(X11 REQUIRED)
+endif()
+
+if(MPE_USE_WAYLAND)
+  find_package(Wayland REQUIRED)
+endif()
+
+if(MPE_USE_X11)
+  if(${VERBOSE})
+    message("
+***********************************************************
+  * fmt
+  * spdlog
+  * glfw3
+  * glm
+  * X11
+***********************************************************")
+  endif()
+elseif(MPE_USE_WAYLAND)
+  if(${VERBOSE})
+    message("
+***********************************************************
+  * fmt
+  * spdlog
+  * glfw3
+  * glm
+  * Wayland
+***********************************************************")
+  endif()
+else()
+  if(${VERBOSE})
     message("
 ***********************************************************
   * fmt
@@ -11,4 +42,5 @@ if(${VERBOSE})
   * glfw3
   * glm
 ***********************************************************")
+  endif()
 endif()
