e018373431a20d772851c65840e2f58858628e41
Sebastian <Sebastian.Termen@gmail.com>
2024-08-13 20:25:08 +0300

    textures can return their IDs and can be explicitly unbound
    added more explicit logging to OpenGLTexture2D

--- MPE/Renderer/Textures/Texture.h
+++ MPE/Renderer/Textures/Texture.h
@@ -11,9 +11,11 @@ class MPE_API Texture
 
     virtual uint32_t GetWidth() const = 0;
     virtual uint32_t GetHeight() const = 0;
+    virtual uint32_t GetID() const = 0;
 
     virtual void SetData(void* data, uint32_t size) = 0;
 
     virtual void Bind(uint32_t slot = 0) const = 0;
+    virtual void Unbind(uint32_t slot = 0) const = 0;
 };
 }
