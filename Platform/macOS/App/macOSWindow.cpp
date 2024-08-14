b7315d98c466d8a698dbbc46c48792feb6616647
Sebastian Termen <sebastian.termen@unity3d.com>
2024-08-14 17:56:32 +0300

    additional glfw hints for OSX using opengl 4.1

--- Platform/macOS/App/macOSWindow.cpp
+++ Platform/macOS/App/macOSWindow.cpp
@@ -66,6 +66,8 @@ void macOSWindow::Init(const WindowProps &props)
     {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     }
 
 #ifdef MPE_ENABLE_DEBUG_LOG
