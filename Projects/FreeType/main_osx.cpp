053d12da8ef46bed43c407c0c24b928a265f35d9
Sebastian Termen <sebastian.termen@unity3d.com>
2024-08-14 17:16:29 +0300

    bumped freetype osx example opengl version to 4.1

--- Projects/FreeType/main_osx.cpp
+++ Projects/FreeType/main_osx.cpp
@@ -41,8 +41,8 @@ int main(int argc, char* argv[])
 
     glfwInit();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
