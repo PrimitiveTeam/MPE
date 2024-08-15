f5f5830f35570b418b66fc6e0de10e2c0457c826
Sebastian <Sebastian.Termen@gmail.com>
2024-08-15 17:53:54 +0300

    using built-in debug menu for Pong ProjectPicker

--- Projects/Pong/ProjectSelector.cpp
+++ Projects/Pong/ProjectSelector.cpp
@@ -2,145 +2,12 @@
 #include "MPE/EntryPoint.h"
 
 #ifdef MPE_OPENGL
-#    include "Platform/OpenGL/OpenGLSettings.h"
+#    include "MPE/MPEGFX_OPEN_GL.h"
+#    include "Platform/OpenGL/Editor/Utilities/OpenGLDebugGuiLayer.h"
+#elif MPE_OPENGLES
+#    include "MPE/MPEGFX_OPEN_GL_ES.h"
+#    include "Platform/OpenGLES/Editor/Utilities/OpenGLESDebugGuiLayer.h"
 #endif
-#ifdef MPE_OPENGLES
-#    include "Platform/OpenGLES/OpenGLESSettings.h"
-#endif
-
-class DebugGuiLayer : public MPE::Layer
-{
-  public:
-    DebugGuiLayer() : Layer("DebugGuiLayer") {}
-
-    void OnAttach() override {}
-
-    void OnDetach() override {}
-
-    void OnUpdate(MPE::Time deltaTime) override {}
-
-    void OnImGuiRender() override
-    {
-        ImGui::Begin("Main Menu", nullptr, ImGuiWindowFlags_NoSavedSettings);
-
-        DebugMenu();
-
-        GraphicsSettings();
-
-        DisplayRefs();
-
-        ImGui::End();
-    }
-
-    void OnEvent(MPE::Event& event) override {}
-
-  private:
-    void DebugMenu()
-    {
-        ImGui::Separator();
-
-        // GetFPS_MS from App/Renderer
-        // Display FPS/ms
-        auto fps_ms = MPE::App::GetFPS_MS();
-        ImGui::Text("FPS: %.1f", fps_ms.FPS);
-        ImGui::Text("MS: %.3f", fps_ms.MS);
-    }
-
-    void GraphicsSettings()
-    {
-        ImGui::Separator();
-
-        ImGui::Text("Graphics Settings");
-        // Retrieve RenderSettings
-        auto api = MPE::RendererAPI::GetGraphicsAPI();
-        switch (api)
-        {
-            case MPE::RendererAPI::API::OpenGL:
-                ImGui::Text("Graphics API: OpenGL");
-                break;
-            case MPE::RendererAPI::API::OpenGLES:
-                ImGui::Text("Graphics API: OpenGLES");
-                break;
-            default:
-                MPE_CORE_ASSERT(false, "NO RENDERER API SELECTED.");
-        }
-        // auto settings = dynamic_cast<MPE::OpenGLSettings*>(MPE::RenderPrimitive::GetSettings());
-        auto settings = MPE::RenderPrimitive::GetSettings();
-        // Display RenderSettings (vsync, blend, depthTest)
-
-        ImGui::Text("Current Vsync: %s", settings->GetVsync() ? "Enabled" : "Disabled");
-
-        bool vsync = settings->GetVsync();
-        if (ImGui::Checkbox("VSync", &vsync))
-        {
-            settings->SetVsync(vsync);
-        }
-
-        bool limitFPS = settings->GetLimitFPS();
-        if (ImGui::Checkbox("Limit FPS", &limitFPS))
-        {
-            settings->SetLimitFPS(limitFPS);
-        }
-
-        int maxFPS = settings->GetMaxFPS();
-        if (ImGui::SliderInt("Max FPS", &maxFPS, 1, 120))
-        {
-            settings->SetMaxFPS(maxFPS);
-        }
-
-        bool blend = settings->GetBlend();
-        if (ImGui::Checkbox("Blend", &blend))
-        {
-            settings->SetBlend(blend);
-        }
-
-        bool depthTest = settings->GetDepthTest();
-        if (ImGui::Checkbox("Depth Test", &depthTest))
-        {
-            settings->SetDepthTest(depthTest);
-        }
-
-        if (api == MPE::RendererAPI::API::OpenGL)
-        {
-#ifdef MPE_OPENGL
-            bool polygonMode = dynamic_cast<MPE::OpenGLSettings*>(settings)->GetPolygonMode();
-            if (ImGui::Checkbox("Polygon Mode", &polygonMode))
-            {
-                dynamic_cast<MPE::OpenGLSettings*>(settings)->SetPolygonMode(polygonMode);
-            }
-#endif
-        }
-    }
-
-    void DisplayRefs()
-    {
-        ImGui::Separator();
-
-        ImGui::Text("Total References: %d", MPE::ReferenceTracker::getInstance().GetTotalReferences());
-
-        if (ImGui::BeginListBox("References"))
-        {
-            auto refs = MPE::ReferenceTracker::getInstance().GetReferences();
-            for (const auto& ref : refs)
-            {
-                ImGui::Text(ref.c_str());
-            }
-            ImGui::EndListBox();
-        }
-
-        if (ImGui::BeginListBox("Scope References"))
-        {
-            auto refs = MPE::ReferenceTracker::getInstance().GetScopeReferences();
-            for (const auto& ref : refs)
-            {
-                ImGui::Text(ref.c_str());
-            }
-            ImGui::EndListBox();
-        }
-    }
-
-  private:
-};
 
 class ProjectSelector : public MPE::App
 {
@@ -150,7 +17,11 @@ class ProjectSelector : public MPE::App
 #ifdef MPE_DYNAMIC_LIBRARY
         ImGui::SetCurrentContext(this->GetImGuiContext());
 #endif
-        PushOverlay(MPE::NEWREF<DebugGuiLayer>());
+#ifdef MPE_OPENGL
+        PushLayer(MPE::NEWREF<MPE::OpenGLDebugGuiLayer>());
+#elif MPE_OPENGLES
+        PushLayer(MPE::NEWREF<MPE::OpenGLESDebugGuiLayer>());
+#endif
         PushLayer(MPE::NEWREF<PONG::Pong>());
     }
 
