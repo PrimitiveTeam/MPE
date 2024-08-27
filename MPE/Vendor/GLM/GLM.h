559915ef9db54142d1112c229f9186e992b8ef16
Sebastian Termen <sebastian.termen@unity3d.com>
2024-08-27 19:22:03 +0300

    make sure to not include glm/ext.hpp if on OSX

--- MPE/Vendor/GLM/GLM.h
+++ MPE/Vendor/GLM/GLM.h
@@ -8,7 +8,11 @@
 #include <glm/glm.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+#if !defined(MPE_PLATFORM_OSX)
 #include <glm/ext.hpp>
+#endif
+
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtx/string_cast.hpp>
